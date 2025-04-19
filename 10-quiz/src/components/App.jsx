ce5515d0fcf1f22de7d97c128643427d6c3b291c
giang-ngo <giangngo.py@gmail.com>
2025-04-19 16:40:45 +0700

    Project 10 - part 1

---
+++ 10-quiz/src/components/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useReducer, useState } from 'react';
+import '../App.css';
+import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+
+const initialState = {
+    questions: [],
+    status: 'loading',
+};
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived':
+            return { ...state, questions: action.payload, status: 'ready' };
+
+        case 'dataFailed':
+            return { ...state, status: 'error' };
+        default:
+            throw new Error('Actions unknown');
+    }
+}
+
+function App() {
+    const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+    const numQuestions = questions.length;
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/questions`)
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+            .catch(() => dispatch({ type: 'dataFailed' }));
+    }, []);
+
+    return (
+        <div className='app'>
+            <Header />
+            <Main>
+                {status === 'loading' && <Loader />}
+                {status === 'error' && <Error />}
+                {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+            </Main>
+        </div>
+    );
+}
+
+export default App;
